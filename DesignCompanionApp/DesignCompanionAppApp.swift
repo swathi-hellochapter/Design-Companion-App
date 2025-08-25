8ad63ef07d5ecc9b8b692df24a733568bb7daf0e
Sri Swathi Nimmagadda <s.swathi@hellochapter.com>
2025-08-26 00:13:22 +0530

    Initial Commit

---
+++ DesignCompanionApp/DesignCompanionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCompanionAppApp.swift
+//  DesignCompanionApp
+//
+//  Created by Sri Swathi Nimmagadda on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignCompanionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DesignCompanionAppTests/DesignCompanionAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  DesignCompanionAppTests.swift
+//  DesignCompanionAppTests
+//
+//  Created by Sri Swathi Nimmagadda on 8/26/25.
+//
+
+import Testing
+
+struct DesignCompanionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
